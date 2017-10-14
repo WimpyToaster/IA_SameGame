def4f009875bcb8058a8156fb4b152bef64b90bf
Varios997 <Varios997@users.noreply.github.com>
2017-10-14 17:50:10 +0100

    Fixed find groups

--- Aich.py
+++ Aich.py
@@ -12,22 +12,24 @@ def color (c):
 # TAI pos 
 # Tuplo (l, c) 
 def  make_pos (l, c): 
-    return (l, c) 
+    return (l, c)
 def pos_l (pos): 
     return pos[0] 
 def pos_c (pos): 
     return pos[1]
 
+def checkIn(el, list):
+	for x in list:
+		if el in x:
+			return True
+	return False
+
 
 def board_find_groups(board):
 	result = []
-	temp = False
 	for y in range(len(board)):
 		for x in range(len(board[0])):
-			for z in range(len(result)):
-				if [x,y] in result[z]:
-					temp = True
-			if not(temp):
+			if not(checkIn(make_pos(x, y), result)):
 				result.append( board_aux_find_group(board, [], x, y, board[y][x]) );
 	return result
 
@@ -35,31 +37,31 @@ def board_aux_find_group(board, curList, x, y, color):
 	if x < 0 or x >= len(board[0]) or y < 0 or y >= len(board):
 		return curList
 
-	if (board[y][x] == color) and not([x,y] in curList):
+	if (board[y][x] == color) and not(make_pos(x, y) in curList):
 		# UP
-		curList.append([x,y])
+		curList.append(make_pos(x, y))
 		print(curList)
 		print
 		print
 		print
 
-		if not([x, y-1] in curList):
+		if not(make_pos(x, y-1) in curList):
 			print("UP")
-			curList.extend( board_aux_find_group(board, curList, x, y-1, color) )
+			curList = board_aux_find_group(board, curList, x, y-1, color)
 		# DWN
-		if not([x, y+1] in curList):
+		if not(make_pos(x, y+1) in curList):
 			print("DWN")
-			curList.extend( board_aux_find_group(board, curList, x, y+1, color) )
+			curList = board_aux_find_group(board, curList, x, y+1, color)
 		# LFT
 		#print([x-1, y] in curList)
-		if not([x-1, y] in curList):
+		if not(make_pos(x-1, y) in curList):
 			print("LFT")
-			curList.extend( board_aux_find_group(board, curList, x-1, y, color) )
+			curList = board_aux_find_group(board, curList, x-1, y, color)
 		# RGT
 		#print([x+1, y] in curList)
-		if not([x+1, y] in curList):
+		if not(make_pos(x+1, y) in curList):
 			print("RGT")
-			curList.extend( board_aux_find_group(board, curList, x+1, y, color) )
+			curList = board_aux_find_group(board, curList, x+1, y, color)
 
 
 	return curList
@@ -69,5 +71,12 @@ board = [[2,2,1,2,1],[2,2,2,1,2],[1,2,1,2,1],[2,1,2,1,2]]
 a = [[1,2]]
 #print([1,2] in a)
 
+print("hello")
+print board_find_groups(board)
+print("ended")
 
-board_find_groups(board)
+
+class sg_state:
+
+	def __init__(self, board):
+		this.board = board
